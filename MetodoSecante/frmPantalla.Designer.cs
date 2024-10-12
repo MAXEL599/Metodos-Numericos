46688973f136a7b145297df12ecd1c8a2e54a27c
Maxel 599 <carlosururchurtu@gmail.com>
2024-10-12 11:36:24 -0700

    Agregar archivos de proyecto.

---
+++ MetodoSecante/frmPantalla.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace MetodoSecante
+{
+    partial class frmPantalla
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblFuncion = new System.Windows.Forms.Label();
+            this.lblX0 = new System.Windows.Forms.Label();
+            this.txtFuncion = new System.Windows.Forms.TextBox();
+            this.txtX0 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.lblTolerancia = new System.Windows.Forms.Label();
+            this.txtTolerancia = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIteraciones = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(255, 28);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(482, 32);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Calculadora Método de la Secante";
+            this.lblTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblFuncion
+            // 
+            this.lblFuncion.AutoSize = true;
+            this.lblFuncion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFuncion.Location = new System.Drawing.Point(22, 160);
+            this.lblFuncion.Name = "lblFuncion";
+            this.lblFuncion.Size = new System.Drawing.Size(135, 25);
+            this.lblFuncion.TabIndex = 1;
+            this.lblFuncion.Text = "Función f(x):";
+            this.lblFuncion.Click += new System.EventHandler(this.lblFuncion_Click);
+            // 
+            // lblX0
+            // 
+            this.lblX0.AutoSize = true;
+            this.lblX0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX0.Location = new System.Drawing.Point(22, 235);
+            this.lblX0.Name = "lblX0";
+            this.lblX0.Size = new System.Drawing.Size(154, 25);
+            this.lblX0.TabIndex = 2;
+            this.lblX0.Text = "Valor Inicial x0";
+            // 
+            // txtFuncion
+            // 
+            this.txtFuncion.Location = new System.Drawing.Point(27, 188);
+            this.txtFuncion.Multiline = true;
+            this.txtFuncion.Name = "txtFuncion";
+            this.txtFuncion.Size = new System.Drawing.Size(212, 30);
+            this.txtFuncion.TabIndex = 3;
+            // 
+            // txtX0
+            // 
+            this.txtX0.Location = new System.Drawing.Point(27, 273);
+            this.txtX0.Multiline = true;
+            this.txtX0.Name = "txtX0";
+            this.txtX0.Size = new System.Drawing.Size(212, 30);
+            this.txtX0.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Valor Inicial x1:";
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(27, 356);
+            this.txtX1.Multiline = true;
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(212, 34);
+            this.txtX1.TabIndex = 6;
+            // 
+            // lblTolerancia
+            // 
+            this.lblTolerancia.AutoSize = true;
+            this.lblTolerancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTolerancia.Location = new System.Drawing.Point(22, 402);
+            this.lblTolerancia.Name = "lblTolerancia";
+            this.lblTolerancia.Size = new System.Drawing.Size(121, 25);
+            this.lblTolerancia.TabIndex = 7;
+            this.lblTolerancia.Text = "Tolerancia:";
+            // 
+            // txtTolerancia
+            // 
+            this.txtTolerancia.Location = new System.Drawing.Point(27, 439);
+            this.txtTolerancia.Multiline = true;
+            this.txtTolerancia.Name = "txtTolerancia";
+            this.txtTolerancia.Size = new System.Drawing.Size(212, 33);
+            this.txtTolerancia.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(392, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Máx. Iteraciones:";
+            // 
+            // txtIteraciones
+            // 
+            this.txtIteraciones.Location = new System.Drawing.Point(397, 188);
+            this.txtIteraciones.Multiline = true;
+            this.txtIteraciones.Name = "txtIteraciones";
+            this.txtIteraciones.Size = new System.Drawing.Size(230, 30);
+            this.txtIteraciones.TabIndex = 10;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(437, 291);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(147, 34);
+            this.btnCalcular.TabIndex = 11;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(392, 406);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(115, 25);
+            this.lblResultado.TabIndex = 12;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(513, 406);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(244, 40);
+            this.txtResultado.TabIndex = 13;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(619, 293);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(154, 31);
+            this.btnLimpiar.TabIndex = 14;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // frmPantalla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 575);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtIteraciones);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTolerancia);
+            this.Controls.Add(this.lblTolerancia);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtX0);
+            this.Controls.Add(this.txtFuncion);
+            this.Controls.Add(this.lblX0);
+            this.Controls.Add(this.lblFuncion);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmPantalla";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPantalla_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblFuncion;
+        private System.Windows.Forms.Label lblX0;
+        private System.Windows.Forms.TextBox txtFuncion;
+        private System.Windows.Forms.TextBox txtX0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.Label lblTolerancia;
+        private System.Windows.Forms.TextBox txtTolerancia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIteraciones;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ MetodoSecante/frmPantalla.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodoSecante
+{
+    public partial class frmPantalla : Form
+    {
+        public frmPantalla()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPantalla_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblFuncion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Parsear entradas desde los TextBox
+                double x0 = double.Parse(txtX0.Text);
+                double x1 = double.Parse(txtX1.Text);
+                double tolerancia = double.Parse(txtTolerancia.Text);
+                int maxIteraciones = int.Parse(txtIteraciones.Text);
+
+                // Definir función
+                Func<double, double> funcion = EvaluarFuncion(txtFuncion.Text);
+
+                // Aplicar el método de la secante
+                double resultado = MetodoSecante(funcion, x0, x1, tolerancia, maxIteraciones);
+
+                // Mostrar resultado
+                txtResultado.Text = resultado.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+
+
+
+        }
+
+
+
+
+
+        private double MetodoSecante(Func<double, double> f, double x0, double x1, double tolerancia, int maxIteraciones)
+        {
+            double xPrevio = x0;
+            double xActual = x1;
+            double xSiguiente;
+
+            for (int i = 0; i < maxIteraciones; i++)
+            {
+                double fXPrevio = f(xPrevio);
+                double fXActual = f(xActual);
+
+                // Comprobar si la diferencia es menor que la tolerancia
+                if (Math.Abs(fXActual) < tolerancia)
+                {
+                    return xActual;
+                }
+
+                // Fórmula del método de la secante
+                xSiguiente = xActual - fXActual * (xActual - xPrevio) / (fXActual - fXPrevio);
+
+                // Comprobar si la diferencia entre iteraciones es menor que la tolerancia
+                if (Math.Abs(xSiguiente - xActual) < tolerancia)
+                {
+                    return xSiguiente;
+                }
+
+                // Actualizar variables para la siguiente iteración
+                xPrevio = xActual;
+                xActual = xSiguiente;
+            }
+
+            throw new Exception("No se encontró la raíz en el número máximo de iteraciones");
+        }
+
+        private Func<double, double> EvaluarFuncion(string funcion)
+        {
+            // Aquí podrías usar una librería como NCalc o escribir un parser simple para la función.
+            // Por ahora, vamos a devolver una función fija como ejemplo (modifícalo según tus necesidades).
+
+            // Ejemplo: Si la función es "x^2 - 2"
+            return x => Math.Pow(x, 2) - 2;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtFuncion.Text = "";
+            txtX0.Text = "";
+            txtX1.Text = "";
+            txtTolerancia.Text = "";
+            txtIteraciones.Text = "";
+            txtResultado.Text = "";
+
+
+        }
+    }
+}
